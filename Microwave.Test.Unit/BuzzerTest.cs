a47f1c53fdd1277ca1d91693d6c1fcd3b96868cf
MadsHoltum <au683315@post.au.dk>
2022-04-20 15:55:02 +0200

    Lavet seperat buzzertest på print

---
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microwave.Classes.Boundary;
+using NUnit.Framework;
+using System.IO;
+using System.Text;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    public class BuzzerTest
+    {
+        private StringWriter readConsole;
+        private Buzzer uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Buzzer();
+            readConsole = new StringWriter();
+            System.Console.SetOut(readConsole);
+        }
+
+        [Test]
+        public void BeepThreeTimes_MethodCalled_PrintsToTerminal()
+        {
+            // act
+            uut.BeepThreeTimes();
+
+            // assert
+            var print = readConsole.ToString();
+            Assert.AreEqual("Buzzer says: Beep, beep, beep\r\n", print);
+        }
+    }
+}
